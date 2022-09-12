ba3282ceb8d74f9089eb919be3623cfdabff61e9
Hashim-Jan <hashimjan033@gmail.com>
2022-09-12 18:28:07 +0500

    function completed

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,9 @@ const database = admin.firestore();
 exports.rideNotification = functions.firestore.document("Rides/{rideId}")
     .onUpdate(async (change, context) => {
         console.log("ride update function called");
-        console.log("Ride id ===>", change.id);
+        // console.log("Ride id ===>", change.id);
         // the previous value before this update
-        const previousValue = change.id;
+        const previousValue = change.before.data();
 
         // Get an object representing the current document
         const ride = change.after.data();
@@ -18,17 +18,19 @@ exports.rideNotification = functions.firestore.document("Rides/{rideId}")
 
         if(ride.isRideCancelled == true) {
             console.log("ride cancelled");
-
-            // var passengers = await database.collection("Rides").doc(change.document.id).get();
-            const userData = await database.collection("app_user").doc(ride.postById).get();
-            sendNotification(userData.data().fcmToken);
-
+            const rider = await database.collection("app_user").doc(ride.postById).get();
+            var passengers = await database.collection("Rides").doc(ride.rideId).collection("Passengers").get();
+            for (let index = 0; index < passengers.length; index++) {
+                const userData = await database.collection("app_user").doc(passengers[index].id).get();
+                sendNotification(userData.data().fcmToken, rider.data());
+                // const element = array[index];     
+            }
         }
-        
 
-        function sendNotification(notificationToken) {
+
+        function sendNotification(notificationToken, rider) {
             const title = "Ride";
-            const body = "The ride joined with user was cancelled";
+            const body = "The ride joined with "+ rider.name +" was cancelled";
             const payload = {
                 notification: { title: title, body: body},
                 token: notificationToken,  
